fe05b6138ac2098eccd07237ad5b74585f96e7d5
rinoffi <sivaneshoffi@gmail.com>
2025-05-30 16:30:51 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -6,16 +6,16 @@ load_dotenv()
 
 class Config:
     # Telegram API Credentials
-    API_ID = int(os.getenv("API_ID"))
-    API_HASH = os.getenv("API_HASH")
-    BOT_TOKEN = os.getenv("BOT_TOKEN")
+    API_ID = int(os.getenv("28015531"))
+    API_HASH = os.getenv("2ab4ba37fd5d9ebf1353328fc915ad28")
+    BOT_TOKEN = os.getenv("7965807385:AAFZiAyeZ4-tbWcw_bgDGwO-gx2Dr1A4x74")
 
     # MongoDB Configuration
-    MONGO_URI = os.getenv("MONGO_URI")
+    MONGO_URI = os.getenv("mongodb+srv://haribotx:haribotx@cluster0.i3skil4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 
     # Bot Configuration
-    SUDO_USERS = [int(x) for x in os.getenv("SUDO_USERS", "").split()]
-    LOG_GROUP_ID = int(os.getenv("LOG_GROUP_ID"))
+    SUDO_USERS = [int(x) for x in os.getenv("7874828505", "").split()]
+    LOG_GROUP_ID = int(os.getenv("-1002365504358"))
 
     # Optional: Genius API for lyrics
     GENIUS_API_TOKEN = os.getenv("GENIUS_API_TOKEN")
